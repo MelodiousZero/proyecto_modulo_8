dc16e7d754b3e894c385dcd2f435ac5fbe172e03
yamorza <raul.llamosas182@gmail.com>
2026-09-09 12:51:51 -0600

    cargar data

---
+++ src/utils/load.R
@@ -0,0 +1,171 @@
+library(EIAapi)
+library(dotenv)
+library(httr)
+library(jsonlite)
+# Pulling the API key from my renviron file
+
+
+load_dot_env(file = ".env")
+
+api_key <- Sys.getenv("eia_key")
+
+list_api_paths <- list("electricity/rto/region-data/data/", #Electric Power Operations Daily and Hourly - Hourly demand, Demand Forecast, Generation and interchange
+                   "electricity/rto/region-sub-ba-data/data/", #Electric Power Operations Daily and Hourly - Hourly demand by subregion
+                   "electricity/rto/fuel-type-data/data/", #Electric Power Operations Daily and Hourly - Hourly Generation by Energy Source
+                   "electricity/rto/interchange-data/data/",#Electric Power Operations Daily and Hourly - Hourly Interchange by Neighboring Balancing Authority
+                   "electricity/state-electricity-profiles/source-disposition/data/", #State Specific Data - Supply and disposition of electricity
+                   "electricity/operating-generator-capacity/data/", #Electricity - Inventory of Operable Genrators 
+                   "electricity/state-electricity-profiles/capability/data/" #Electricity - State specific data generating capacity
+)
+
+
+
+
+# Function to fetch and save EIA data
+fetch_and_save_eia_data <- function(path_index, csv_filename) {
+  # Fetch the data
+  df <- eia_get(
+    api_key = api_key,
+    api_path = list_api_paths[[path_index]],
+    data = "value"
+  )
+  
+  # Save to CSV
+  write.csv(df, csv_filename, row.names = FALSE)
+  
+  # Return the data frame (optional)
+  return(df)
+}
+
+# Usage
+df1 <- fetch_and_save_eia_data(
+  path_index = 1,
+  csv_filename = "daily_hourly_all.csv"
+)
+
+
+
+df3 <- fetch_and_save_eia_data(
+  path_index = 3,
+  csv_filename = "hourly_generation_by_energy_source.csv"
+)
+
+df4 <- fetch_and_save_eia_data(
+  path_index = 4,
+  csv_filename = "hourly_interchange.csv"
+)
+
+# solo los de arriba funcionaron con la paqueteria de eapi
+
+#hourly demand by subregion
+response <- GET(
+  "https://api.eia.gov/v2/electricity/rto/region-sub-ba-data/data/",
+  query = list(
+    api_key = api_key,
+    frequency = "hourly",
+    `data[0]` = "value",
+    `sort[0][column]` = "period",
+    `sort[0][direction]` = "desc",
+    offset = 0,
+    length = 5000
+  )
+)
+
+status_code(response)  # Should be 200
+
+parsed <- fromJSON(content(response, "text"))
+
+df <- parsed$response$data
+
+
+write.csv(df, "hourly_demand_subregion.csv", row.names = FALSE)
+
+
+#State Supply Electricity
+respuesta <- GET(
+  "https://api.eia.gov/v2/electricity/state-electricity-profiles/source-disposition/data/",
+  query = list(
+    api_key = api_key,
+    frequency = "annual",
+    `data[0]` = "combined-heat-and-pwr-comm",
+    `data[1]` = "combined-heat-and-pwr-elect",
+    `data[2]` = "combined-heat-and-pwr-indust",
+    `data[3]` = "direct-use",
+    `data[4]` = "elect-pwr-sector-gen-subtotal",
+    `data[5]` = "electric-utilities",
+    `data[6]` = "energy-only-providers",
+    `data[7]` = "estimated-losses",
+    `data[8]` = "facility-direct",
+    `data[9]` = "full-service-providers",
+    `data[10]` = "independent-power-producers",
+    `data[11]` = "indust-and-comm-gen-subtotal",
+    `data[12]` = "net-interstate-trade",
+    `data[13]` = "net-trade-index",
+    `data[14]` = "total-disposition",
+    `data[15]` = "total-elect-indust",
+    `data[16]` = "total-international-exports",
+    `data[17]` = "total-international-imports",
+    `data[18]` = "total-net-generation",
+    `data[19]` = "total-supply",
+    `data[20]` = "unaccounted",
+    `sort[0][column]` = "period",
+    `sort[0][direction]` = "desc",
+    offset = 0,
+    length = 5000
+  )
+)
+
+datos <- fromJSON(content(respuesta, "text"))$response$data
+write.csv(datos, "source_disposition_data.csv", row.names = FALSE)
+
+
+#Operable Generators
+respuesta <- GET(
+  "https://api.eia.gov/v2/electricity/operating-generator-capacity/data/",
+  query = list(
+    api_key = api_key,
+    frequency = "monthly",
+    `data[0]` = "county",
+    `data[1]` = "latitude",
+    `data[2]` = "longitude",
+    `data[3]` = "nameplate-capacity-mw",
+    `data[4]` = "net-summer-capacity-mw",
+    `data[5]` = "net-winter-capacity-mw",
+    `data[6]` = "operating-year-month",
+    `data[7]` = "planned-derate-summer-cap-mw",
+    `data[8]` = "planned-derate-year-month",
+    `data[9]` = "planned-retirement-year-month",
+    `data[10]` = "planned-uprate-summer-cap-mw",
+    `data[11]` = "planned-uprate-year-month",
+    `sort[0][column]` = "period",
+    `sort[0][direction]` = "desc",
+    offset = 0,
+    length = 5000
+  )
+)
+
+datos <- fromJSON(content(respuesta, "text"))$response$data
+write.csv(datos, "operating_generators_capacity.csv", row.names = FALSE)
+
+
+#State gnerating capacity
+respuesta <- GET(
+  "https://api.eia.gov/v2/electricity/state-electricity-profiles/capability/data/",
+  query = list(
+    api_key = api_key,
+    frequency = "annual",
+    `data[0]` = "capability",
+    `sort[0][column]` = "period",
+    `sort[0][direction]` = "desc",
+    offset = 0,
+    length = 5000
+  )
+)
+
+datos <- fromJSON(content(respuesta, "text"))$response$data
+write.csv(datos, "state_generator_capacity.csv", row.names = FALSE)
+
+
+
+
+
